cd904c0ca4b696f70db8303b533e23213ea191cc
KerenMedina <47213202+KerenMedina@users.noreply.github.com>
2019-02-06 10:23:27 +0100

    Add files via upload

---
+++ Ejercicio7.java
@@ -0,0 +1,16 @@
+package ejercicio7;
+/**
+ *
+ * @author Keren
+ */
+public class Ejercicio7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola ");
+    }
+    
+}
