3b45445721fe0be2e13530a1e3f726467d3bae80
KerenMedina; <kerenmedinacoasta@gmail.com>
2019-02-06 11:53:17 +0100

    Añadido hola

--- ejercicio7/src/ejercicio7/Ejercicio7.java
+++ ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -15,6 +15,7 @@ public class Ejercicio7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("Hola");
         System.out.println("Adios");
     }
     
